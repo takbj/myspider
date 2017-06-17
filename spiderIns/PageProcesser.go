33c4ea5e17ddcad353cb1ea283e247075540a91f
zhangshengyang <zhangshengyang@lmdgame.com>
2017-06-17 14:37:33 +0800

    continue

--- spiderIns/PageProcesser.go
+++ spiderIns/PageProcesser.go
@@ -22,24 +22,6 @@ func (this *MyPageProcesser) Finish() {
 	fmt.Printf("TODO:before end spider \r\n")
 }
 
-//func queryCBFun(i int, s *goquery.Selection) {
-//	href, _ := s.Attr("href")
-//	if strings.Index(href, "://") >= 0 {
-//		return
-//	}
-
-//	index := strings.LastIndex(url, "/")
-//	if index == -1 {
-//		index = len(url)
-//	}
-//	path := url[0 : index+1]
-
-//	if _, exist := existUrls[cstUrlHead+href]; !exist {
-//		existUrls[path+href] = true
-//		urls = append(urls, cstUrlHead+href)
-//	}
-//}
-
 // Parse html dom here and record the parse result that we want to Page.
 // Package goquery (http://godoc.org/github50.com/PuerkitoBio/goquery) is used to parse html.
 func (this *MyPageProcesser) Process(p *page.Page) {
@@ -57,26 +39,43 @@ func (this *MyPageProcesser) Process(p *page.Page) {
 
 	lastSepIndex := strings.LastIndex(curUrl.Path, "/")
 	relativePath := curUrl.Path[:lastSepIndex+1]
-	urlPath := fmt.Sprintf("%s://%s%s", curUrl.Scheme, curUrl.Host, relativePath)
+	//	urlDir := fmt.Sprintf("%s://%s%s", curUrl.Scheme, curUrl.Host, relativePath)
 
-	var urls []string
-	var urlTmp string
-	query.Find("a").Each(func(i int, s *goquery.Selection) {
-		href, _ := s.Attr("href")
+	var urls = []string{}
+	cbFun := func(i int, s *goquery.Selection) {
+		href, exist := s.Attr("href")
+		if !exist {
+			href, _ = s.Attr("src")
+		}
+		//		fmt.Println("href=", href)
 		hrefUrl, _ := url.Parse(href)
+		if !hrefUrl.IsAbs() {
+			hrefUrl.Host = curUrl.Host
+			hrefUrl.Path = relativePath + hrefUrl.Path
+			hrefUrl.Scheme = curUrl.Scheme
+		}
 		if ok, exist := allHosts[hrefUrl.Host]; !ok || !exist {
 			return
 		}
+		if hrefUrl.Path == "" || hrefUrl.Path == "\\" || hrefUrl.Path == "/" {
+			hrefUrl.Path = "/index.html"
+		}
 
-		urlTmp = hrefUrl.String()
+		urlTmp := hrefUrl.String()
+		//		fmt.Println("href=", href, "urlTmp=", urlTmp)
 
 		if _, exist := existUrls[urlTmp]; !exist {
-			existUrls[urlPath+href] = true
+			existUrls[urlTmp] = true
 			urls = append(urls, urlTmp)
 		}
-	})
+	}
+
+	query.Find("a").Each(cbFun)
+	query.Find("link").Each(cbFun)
+	query.Find("script").Each(cbFun)
 
 	// these urls will be saved and crawed by other coroutines.
+	fmt.Println("*MyPageProcesser.Process", urls)
 	p.AddTargetRequests(urls, "html")
 }
 
@@ -103,7 +102,9 @@ func getFilePath(curUrl *url.URL) (filePath, fileName string) {
 	pathTmp := []string{cstSaveRootPath, curUrl.Host}
 
 	pathTmp = append(pathTmp, strings.Split(curUrl.Path, "/")...)
+	fmt.Println(pathTmp[:len(pathTmp)-1])
 
 	filePath = path.Join(pathTmp[:len(pathTmp)-1]...)
+	fmt.Println("filePath=", filePath, ",pathTmp[len(pathTmp)-1]=", pathTmp[len(pathTmp)-1])
 	return filePath, pathTmp[len(pathTmp)-1]
 }
